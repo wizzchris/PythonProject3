0d81fb70fe94361599f0fd4f948cff70af348d9f
Christopher Rogers <crogers@spartaglobal.com>
2020-02-05 19:44:23 +0000

    Exercise1

---
+++ Exercise1.py
@@ -0,0 +1,18 @@
+
+bucket_list = ['sand', 'water', 'gravel', 'rocks', 'sea water', 'dirt', 'grass', 'air', 'sticks', 'leaves']
+
+print(bucket_list)
+
+print(len(bucket_list))
+
+print(bucket_list[0])
+
+print(bucket_list[1])
+
+bucket_list.append('fish')
+
+print(bucket_list[-1])
+
+bucket_list[2] = 'stones'
+
+print(bucket_list)
